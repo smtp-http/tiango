65224a102266ca5322d144b2a9403ed052a9cdb3
kevinzu <zukeqiang@foxmail.com>
2019-07-23 17:21:59 +0800

    config & httpserver

---
+++ config/config.go
@@ -0,0 +1,62 @@
+package config 
+
+import (
+    "encoding/json"
+    "io/ioutil"
+   // "fmt"
+  //  "os"
+    "sync"
+)
+
+///////////////////////////////////  configuration ////////////////////////////////////
+
+/*config*/
+type Configuration struct {
+    HttpPort    string  `json:"http_port"`
+    Version     string  `json:"version"`
+}
+
+
+var config *Configuration
+var once_cfg sync.Once
+ 
+func GetConfig() *Configuration {
+    once_cfg.Do(func() {
+        config = &Configuration{}
+    })
+    return config
+}
+
+
+
+//////////////////////////////////////  config loader ///////////////////////////////////
+
+type ConfigLoader struct {
+
+}
+
+
+var loader *ConfigLoader
+var once_loader sync.Once
+ 
+func GetLoader() *ConfigLoader {
+    once_loader.Do(func() {
+        loader = &ConfigLoader{}
+    })
+    return loader
+}
+
+
+
+func (jst *ConfigLoader) Load(filename string, v interface{}) { 
+//ReadFile函数会读取文件的全部内容，并将结果以[]byte类型返回 
+	data, err := ioutil.ReadFile(filename) 
+	if err != nil { 
+		return 
+	} //读取的数据为json格式，需要进行解码 
+
+	err = json.Unmarshal(data, v) 
+	if err != nil { 
+		return 
+	} 
+}
--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import(
 	"fmt"
+	"github.com/smtp-http/tiango/config"
+	"github.com/smtp-http/tiango/api"
 )
 
 
 func main(){
-	fmt.Printf("tiango\n")
+	loader := config.GetLoader()
+	loader.Load("./config.json",config.GetConfig())
+
+	port := config.GetConfig().HttpPort
+
+	fmt.Printf("tiango: %v\n",port)
+
+	api.StartHttpServer()
 }
