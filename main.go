6dfc6438c86e7eea65591351cea882245134a4de
kevinzu <zukeqiang@foxmail.com>
2020-10-12 15:54:41 +0800

    update

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
-import(
+import (
 	"fmt"
-	"github.com/smtp-http/tiango/config"
-	"github.com/smtp-http/tiango/api"
+	"github.com/smtp-http/tiango/src/api"
+	"github.com/smtp-http/tiango/src/config"
 )
 
-
-func main(){
+func main() {
 	loader := config.GetLoader()
-	loader.Load("./config.json",config.GetConfig())
+	loader.Load("./config.json", config.GetConfig())
 
 	port := config.GetConfig().HttpPort
 
-	fmt.Printf("tiango: %v\n",port)
+	fmt.Printf("tiango: %v\n", port)
 
 	server := &api.GinServer{}
 
